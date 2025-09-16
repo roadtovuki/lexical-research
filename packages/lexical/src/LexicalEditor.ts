d64220a05255f35c34a0ca442d057571363ca268
Mateo Vuković <231365949+roadtovuki@users.noreply.github.com>
2025-09-16 19:11:18 +0200

    Start implementing createEditor function

--- packages/lexical/src/LexicalEditor.ts
+++ packages/lexical/src/LexicalEditor.ts
@@ -1,1 +1,24 @@
-export function createEditor() {}
+import { internalGetActiveEditor } from './LexicalUpdates';
+
+export type EditorThemeClasses = {};
+
+export type CreateEditorArgs = {
+  disableEvents?: boolean;
+  parentEditor?: LexicalEditor;
+  theme?: EditorThemeClasses;
+};
+
+export function createEditor(editorConfig?: CreateEditorArgs): LexicalEditor {
+  const config = editorConfig || {};
+  const activeEditor = internalGetActiveEditor();
+  const theme = config.theme || {};
+  const parentEditor =
+    editorConfig === undefined ? activeEditor : config.parentEditor || null;
+  const disableEvents = config.disableEvents || false;
+
+  const editor = new LexicalEditor();
+
+  return editor;
+}
+
+export class LexicalEditor {}
